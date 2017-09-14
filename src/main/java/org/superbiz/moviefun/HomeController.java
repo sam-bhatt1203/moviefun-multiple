3c1cdc19845a4fcc092b923e325b74f28668ab76
Samrat Bhattacharjee <samrat.bhattacharjee@capgemini.com>
2017-09-14 09:02:20 +0530

    first push for multiple datasource

---
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -0,0 +1,63 @@
+package org.superbiz.moviefun;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.superbiz.moviefun.albums.Album;
+import org.superbiz.moviefun.albums.AlbumFixtures;
+import org.superbiz.moviefun.albums.AlbumsBean;
+import org.superbiz.moviefun.movies.Movie;
+import org.superbiz.moviefun.movies.MovieFixtures;
+import org.superbiz.moviefun.movies.MoviesBean;
+
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    private final MoviesBean moviesBean;
+    private final AlbumsBean albumsBean;
+    private final MovieFixtures movieFixtures;
+    private final AlbumFixtures albumFixtures;
+
+    private final PlatformTransactionManager moviesTransactionManager;
+    private final PlatformTransactionManager albumsTransactionManager;
+
+    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures,
+                          @Qualifier("moviesTransactionManager") PlatformTransactionManager moviesTransactionManager,
+                          @Qualifier("albumsTransactionManager") PlatformTransactionManager albumsTransactionManager) {
+        this.moviesBean = moviesBean;
+        this.albumsBean = albumsBean;
+        this.movieFixtures = movieFixtures;
+        this.albumFixtures = albumFixtures;
+        this.moviesTransactionManager = moviesTransactionManager;
+        this.albumsTransactionManager = albumsTransactionManager;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/setup")
+    public String setup(Map<String, Object> model) {
+//Check for Transaction Template. It will enable all things like exception handilings
+        TransactionStatus mTransaction = moviesTransactionManager.getTransaction(null);
+        for (Movie movie : movieFixtures.load()) {
+            moviesBean.addMovie(movie);
+        }
+        moviesTransactionManager.commit(mTransaction);
+
+        TransactionStatus aTransaction = albumsTransactionManager.getTransaction(null);
+        for (Album album : albumFixtures.load()) {
+            albumsBean.addAlbum(album);
+        }
+        albumsTransactionManager.commit(aTransaction);
+        model.put("movies", moviesBean.getMovies());
+        model.put("albums", albumsBean.getAlbums());
+
+        return "setup";
+    }
+}
